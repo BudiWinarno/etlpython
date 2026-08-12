92eaf80502eff1b6630618e1b0c62ef8dfb686d8
BudiWinarno <budiwinarno2@gmail.com>
2026-08-12 15:39:13 +0700

    update 12 agustus 2026

--- app.py
+++ app.py
@@ -17,6 +17,7 @@ from routes.generate_cmo import generate_cmo_bp
 from routes.cmo_template import cmo_template_bp
 from routes.sqlserver_api import sqlserver_bp
 from routes.insell_report import insell_report_bp
+from routes.auth import auth_bp
 
 app = Flask(__name__)
 app.secret_key = "secret123"
@@ -37,6 +38,7 @@ app.register_blueprint(generate_cmo_bp)
 app.register_blueprint(cmo_template_bp)
 app.register_blueprint(sqlserver_bp)
 app.register_blueprint(insell_report_bp)
+app.register_blueprint(auth_bp)
 
 if __name__ == "__main__":
     app.run(debug=True)
--- routes/home_routes.py
+++ routes/home_routes.py
@@ -1,8 +1,20 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, redirect, url_for, session
 
 home_bp = Blueprint("home", __name__)
 
-@home_bp.route("/")
-def index():
+# DASHBOARD BARU
+@home_bp.route("/dashboard")
+def dashboard():
+    if not session.get("logged_in"):
+        return redirect(url_for("auth.login"))
+
+    return render_template("home_baru.html")
+
+
+# DASHBOARD LAMA
+@home_bp.route("/dashboard_lama")
+def dashboard_lama():
+    if not session.get("logged_in"):
+        return redirect(url_for("auth.login"))
 
     return render_template("home.html")
